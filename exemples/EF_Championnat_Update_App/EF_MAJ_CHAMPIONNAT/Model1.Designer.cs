71eda3fb16337a8db0eb3e9aa8a7e6b144bfc4c8
Pierre Ramet <ramet_pierre@hotmail.fr>
2021-05-04 17:04:14 +0200

    Update Framework 4.7.2

--- exemples/EF_Championnat_Update_App/EF_MAJ_CHAMPIONNAT/Model1.Designer.cs
+++ exemples/EF_Championnat_Update_App/EF_MAJ_CHAMPIONNAT/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'C:\Users\ramet\Documents\Source\Repos\m2106_bd_agile\exemples\EF_Championnat_Update_App\EF_MAJ_CHAMPIONNAT\Model1.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'C:\Users\ramet\Source\Repos\m2106_bd_pt_agile\exemples\EF_Championnat_Update_App\EF_MAJ_CHAMPIONNAT\Model1.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
