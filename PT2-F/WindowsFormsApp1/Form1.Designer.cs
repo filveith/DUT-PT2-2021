f847dd986a870dd6fc9cb07e9e75596395657b4d
skhalifa <skhalifa@etu.u-bordeaux.fr>
2021-06-04 14:05:33 +0200

    Ajout menu de base du projet, prêt au branchage + rebasage

--- PT2-F/WindowsFormsApp1/Form1.Designer.cs
+++ PT2-F/WindowsFormsApp1/Form1.Designer.cs
@@ -29,13 +29,66 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.inscription = new System.Windows.Forms.Button();
+            this.connexion = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.inscription, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.connexion, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // inscription
+            // 
+            this.inscription.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.inscription.Location = new System.Drawing.Point(162, 213);
+            this.inscription.Name = "inscription";
+            this.inscription.Size = new System.Drawing.Size(75, 23);
+            this.inscription.TabIndex = 0;
+            this.inscription.Text = "Inscription";
+            this.inscription.UseVisualStyleBackColor = true;
+            // 
+            // connexion
+            // 
+            this.connexion.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.connexion.Location = new System.Drawing.Point(562, 213);
+            this.connexion.Name = "connexion";
+            this.connexion.Size = new System.Drawing.Size(75, 23);
+            this.connexion.TabIndex = 1;
+            this.connexion.Text = "Connexion";
+            this.connexion.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button inscription;
+        private System.Windows.Forms.Button connexion;
     }
 }
 
