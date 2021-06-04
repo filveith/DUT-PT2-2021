b92e7a8c8c25fea04d60a65ee99b259a1d84a9ec
Anna Briançon <anna.briancon44@gmail.com>
2021-06-04 17:37:21 +0200

    ajouter abo

--- PT2-F/WindowsFormsApp1/Form1.cs
+++ PT2-F/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,11 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void inscription_Click(object sender, EventArgs e)
+        {
+            InscriptionView view = new InscriptionView();
+            view.Show();
+        }
     }
 }
