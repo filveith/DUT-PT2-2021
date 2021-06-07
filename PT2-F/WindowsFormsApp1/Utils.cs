8ca0130f5c4b3a6cd701c3961ba3333f3cdfaf83
skhalifa <skhalifa@etu.u-bordeaux.fr>
2021-06-07 09:53:27 +0200

    Refactoring

--- PT2-F/WindowsFormsApp1/Utils.cs
+++ PT2-F/WindowsFormsApp1/Utils.cs
@@ -37,10 +37,10 @@ namespace WindowsFormsApp1
         public static List<EMPRUNTER> AvoirLesEmpruntProlonger()
         {
             List<EMPRUNTER> result = (from emp in Connexion.EMPRUNTER
-                          join abo in Connexion.ABONNÉS on emp.CODE_ABONNÉ equals abo.CODE_ABONNÉ
-                          join alb in Connexion.ALBUMS on emp.CODE_ALBUM equals alb.CODE_ALBUM
-                          where emp.nbRallongements > 0
-                          select emp).ToList();
+                                      join abo in Connexion.ABONNÉS on emp.CODE_ABONNÉ equals abo.CODE_ABONNÉ
+                                      join alb in Connexion.ALBUMS on emp.CODE_ALBUM equals alb.CODE_ALBUM
+                                      where emp.nbRallongements > 0
+                                      select emp).ToList();
 
                 
             return result;
@@ -68,33 +68,34 @@ namespace WindowsFormsApp1
                                   where empDept.Count() == 0 && DbFunctions.DiffDays(a.creationDate, DateTime.Now) > 365
                                   select a);
             var abosDejaEmprunt = (from a in Connexion.ABONNÉS
-                         join e in Connexion.EMPRUNTER
-                         on a.CODE_ABONNÉ equals e.CODE_ABONNÉ
-                         where DbFunctions.DiffDays(e.DATE_EMPRUNT, DateTime.Now) > 365
-                         select a).Union(abosPasEmprunt).ToList().Distinct();
+                                   join e in Connexion.EMPRUNTER
+                                   on a.CODE_ABONNÉ equals e.CODE_ABONNÉ
+                                   where DbFunctions.DiffDays(e.DATE_EMPRUNT, DateTime.Now) > 365
+                                   select a).Union(abosPasEmprunt).ToList().Distinct();
 
             var abos = abosDejaEmprunt.Union(abosPasEmprunt).ToList();
 
             return abos;
         }
 
-        public static void prolongerTousEmprunts(int codeAbonne)
+        public static List<EMPRUNTER> prolongerTousEmprunts(int codeAbonne)
         {
             int i = 0;
-            var emprunts = from emp in Connexion.EMPRUNTER
+            var emprunts = (from emp in Connexion.EMPRUNTER
                            where emp.CODE_ABONNÉ == codeAbonne
-                           select emp;
+                           where emp.nbRallongements == 0
+                           select emp).ToList();
+
 
             foreach (EMPRUNTER e in emprunts)
             {
                 i++;
-                if (e.nbRallongements != 1)
-                {
-                    e.DATE_RETOUR_ATTENDUE = e.DATE_RETOUR_ATTENDUE.AddMonths(1);
-                    e.nbRallongements = 1;
-                }
+                e.DATE_RETOUR_ATTENDUE = e.DATE_RETOUR_ATTENDUE.AddMonths(1);
+                e.nbRallongements = 1;
             }
+            Connexion.SaveChanges();
             Console.WriteLine(i + " rallongement(s) effectué(s)");
+            return emprunts;
         }
 
         public static Dictionary<EMPRUNTER, ABONNÉS> ConsulterEmprunts(string login)
@@ -106,15 +107,15 @@ namespace WindowsFormsApp1
             var emprunt = (from alb in Connexion.ALBUMS
                            join emp in Connexion.EMPRUNTER on alb.CODE_ALBUM equals emp.CODE_ALBUM
                            join abo in Connexion.ABONNÉS on emp.CODE_ABONNÉ equals abo.CODE_ABONNÉ
-                           where abo.CODE_ABONNÉ == abonne.CODE_ABONNÉ
+                           where abo.LOGIN_ABONNÉ == login
                            orderby emp.DATE_RETOUR_ATTENDUE ascending
-                           select emp).ToList();
+                           select new { emprunt = emp, abonne = abo }).ToList();
 
 
 
             foreach (var al in emprunt)
             {
-                emprunts.Add(al, abonne);
+                emprunts.Add(al.emprunt, al.abonne);
                 //Console.WriteLine(em) ;
             }
             return emprunts;
