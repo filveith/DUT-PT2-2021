cb8c535ddc8e8e964e077f5fb6c859f83eaa2eef
augcolas <augcolas@INFO-210-24>
2021-06-07 16:48:37 +0200

    interface Connexion terminée

--- PT2-F/WindowsFormsApp1/ConnexionView.Designer.cs
+++ PT2-F/WindowsFormsApp1/ConnexionView.Designer.cs
@@ -29,96 +29,139 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.validerButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.idTextBox = new System.Windows.Forms.TextBox();
             this.passTextBox = new System.Windows.Forms.TextBox();
-            this.ConnexionText = new System.Windows.Forms.Label();
-            this.IdLabel = new System.Windows.Forms.Label();
-            this.passLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // validerButton
+            // tableLayoutPanel1
             // 
-            this.validerButton.Location = new System.Drawing.Point(336, 324);
-            this.validerButton.Name = "validerButton";
-            this.validerButton.Size = new System.Drawing.Size(102, 33);
-            this.validerButton.TabIndex = 0;
-            this.validerButton.Text = "Valider";
-            this.validerButton.UseVisualStyleBackColor = true;
-            this.validerButton.Click += new System.EventHandler(this.validerButton_Click);
-            this.validerButton.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 105F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 97F));
+            this.tableLayoutPanel1.Controls.Add(this.idTextBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.passTextBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.21306F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.78694F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 103F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 146F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(687, 524);
+            this.tableLayoutPanel1.TabIndex = 6;
             // 
             // idTextBox
             // 
-            this.idTextBox.Location = new System.Drawing.Point(288, 148);
+            this.idTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.idTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.idTextBox.Font = new System.Drawing.Font("Miriam Libre", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idTextBox.Location = new System.Drawing.Point(239, 226);
             this.idTextBox.Name = "idTextBox";
-            this.idTextBox.Size = new System.Drawing.Size(212, 20);
-            this.idTextBox.TabIndex = 1;
+            this.idTextBox.Size = new System.Drawing.Size(212, 32);
+            this.idTextBox.TabIndex = 5;
             this.idTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             // 
             // passTextBox
             // 
-            this.passTextBox.Location = new System.Drawing.Point(288, 212);
+            this.passTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.passTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.passTextBox.Font = new System.Drawing.Font("Miriam Libre", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passTextBox.Location = new System.Drawing.Point(239, 315);
             this.passTextBox.Name = "passTextBox";
             this.passTextBox.PasswordChar = '*';
-            this.passTextBox.Size = new System.Drawing.Size(212, 20);
-            this.passTextBox.TabIndex = 2;
+            this.passTextBox.Size = new System.Drawing.Size(212, 36);
+            this.passTextBox.TabIndex = 6;
             this.passTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             // 
-            // ConnexionText
+            // button1
             // 
-            this.ConnexionText.AutoSize = true;
-            this.ConnexionText.Location = new System.Drawing.Point(361, 106);
-            this.ConnexionText.Name = "ConnexionText";
-            this.ConnexionText.Size = new System.Drawing.Size(57, 13);
-            this.ConnexionText.TabIndex = 3;
-            this.ConnexionText.Text = "Connexion";
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Miriam Libre", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(220, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(249, 82);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Valider";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             // 
-            // IdLabel
+            // label2
             // 
-            this.IdLabel.AutoSize = true;
-            this.IdLabel.Location = new System.Drawing.Point(215, 151);
-            this.IdLabel.Name = "IdLabel";
-            this.IdLabel.Size = new System.Drawing.Size(53, 13);
-            this.IdLabel.TabIndex = 4;
-            this.IdLabel.Text = "Identifiant";
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Miriam Libre", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(43, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 42);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Mot de passe";
             // 
-            // passLabel
+            // label3
             // 
-            this.passLabel.AutoSize = true;
-            this.passLabel.Location = new System.Drawing.Point(197, 215);
-            this.passLabel.Name = "passLabel";
-            this.passLabel.Size = new System.Drawing.Size(71, 13);
-            this.passLabel.TabIndex = 5;
-            this.passLabel.Text = "Mot de passe";
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Miriam Libre", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(12, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Identifiant";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Miriam Libre", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(164, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 84);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Connexion";
             // 
             // ConnexionView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.passLabel);
-            this.Controls.Add(this.IdLabel);
-            this.Controls.Add(this.ConnexionText);
-            this.Controls.Add(this.passTextBox);
-            this.Controls.Add(this.idTextBox);
-            this.Controls.Add(this.validerButton);
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(711, 548);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "ConnexionView";
             this.Text = "ConnexionView";
             this.Load += new System.EventHandler(this.ConnexionView_Load);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button validerButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox idTextBox;
         private System.Windows.Forms.TextBox passTextBox;
-        private System.Windows.Forms.Label ConnexionText;
-        private System.Windows.Forms.Label IdLabel;
-        private System.Windows.Forms.Label passLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
