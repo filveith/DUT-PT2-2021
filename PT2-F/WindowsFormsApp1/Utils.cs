4cde9c67e1cabf75308c154844e317e26ed6a9c0
skhalifa <skhalifa@etu.u-bordeaux.fr>
2021-06-07 09:43:14 +0200

    Rebasage + mise en static de toutes les méthodes

--- PT2-F/WindowsFormsApp1/Utils.cs
+++ PT2-F/WindowsFormsApp1/Utils.cs
@@ -11,7 +11,7 @@ namespace WindowsFormsApp1
     {
         private static MusiquePT2_FEntities Connexion = new MusiquePT2_FEntities();
 
-        public List<ABONNÉS> AvoirAbonneAvecEmpruntRetardDe10Jours()
+        public static List<ABONNÉS> AvoirAbonneAvecEmpruntRetardDe10Jours()
         {
             List<ABONNÉS> result = new List<ABONNÉS>();
             var emprunt = from emp in Connexion.EMPRUNTER
@@ -29,28 +29,6 @@ namespace WindowsFormsApp1
                                 where alb.CODE_ALBUM == e.CODE_ALBUM
                                 select alb).First();
 
-                Console.WriteLine(abonne.PRÉNOM_ABONNÉ + " " + abonne.NOM_ABONNÉ + " " + album.TITRE_ALBUM);
-            }
-            return result;
-        }
-        public List<ABONNÉS> AvoirAbonneAvecEmpruntRetardDe10Jours()
-        {
-            List<ABONNÉS> result = new List<ABONNÉS>();
-            var emprunt = from emp in Connexion.EMPRUNTER
-                          where emp.DATE_RETOUR == null && DbFunctions.DiffDays(emp.DATE_RETOUR_ATTENDUE, DateTime.Now) > 10
-                          select emp;
-
-            foreach (EMPRUNTER e in emprunt)
-            {
-
-                ABONNÉS abonne = (from abo in Connexion.ABONNÉS
-                                  where abo.CODE_ABONNÉ == e.CODE_ABONNÉ
-                                  select abo).First();
-                result.Add(abonne);
-
-                ALBUMS album = (from alb in Connexion.ALBUMS
-                                where alb.CODE_ALBUM == e.CODE_ALBUM
-                                select alb).First();
                 Console.WriteLine(abonne.PRÉNOM_ABONNÉ + " " + abonne.NOM_ABONNÉ + " " + album.TITRE_ALBUM);
             }
             return result;
