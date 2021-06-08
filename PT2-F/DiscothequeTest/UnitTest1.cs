908eedb6f5c3924fd906208bf131d63cc48b7465
fveith <fveith@etu.u-bordeaux.fr>
2021-06-08 09:02:20 +0200

    test unitaire

---
+++ PT2-F/DiscothequeTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WindowsFormsApp1;
+
+namespace DiscothequeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public static void TestMethod1()
+        {
+            //Assert.IsFalse(Utils.GetABONNÉ().LOGIN_ABONNÉ.Equals("fveith"));
+            var bd = Utils.RegisterAbo("Fil","Veith","fveith","123456",1);
+        }
+    }
+}
