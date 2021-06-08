01dfb6681453512ab6d9f0e6ed307636408926a7
augcolas <augcolas@INFO-210-24>
2021-06-08 08:56:03 +0200

    changement couleur interface

--- PT2-F/WindowsFormsApp1/ConnexionView.Designer.cs
+++ PT2-F/WindowsFormsApp1/ConnexionView.Designer.cs
@@ -160,7 +160,7 @@ namespace WindowsFormsApp1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(684, 561);
             this.Controls.Add(this.tableLayoutPanel2);
             this.MaximumSize = new System.Drawing.Size(1000, 800);
