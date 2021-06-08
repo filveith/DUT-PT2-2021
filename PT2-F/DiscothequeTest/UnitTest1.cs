7963a75993dfe9c82b7eabe279ecbe8807084363
rtesei <rtesei@INFO-206-24>
2021-06-08 14:59:47 +0200

    j'espere

--- PT2-F/DiscothequeTest/UnitTest1.cs
+++ PT2-F/DiscothequeTest/UnitTest1.cs
@@ -154,7 +154,7 @@ namespace DiscothequeTest
         }
 
         [TestMethod]
-        public void TestUS3()
+        public async void TestUS3()
         {
             Random rand = new Random();
 
@@ -168,7 +168,7 @@ namespace DiscothequeTest
             }
 
             // On crée un abonné pour nos tests
-            Utils.RegisterAbo("Test", "US3", "tus3", "mdpStrong", 45);            
+            await Utils.RegisterAbo("Test", "US3", "tus3", "mdpStrong", 45);            
 
             Assert.IsTrue(abo != null);
 
@@ -177,7 +177,7 @@ namespace DiscothequeTest
             ALBUMS alToTake = (from ab in Utils.Connexion.ALBUMS
                                where ab.CODE_ALBUM == ind
                                select ab).FirstOrDefault();
-            
+
             abo.Emprunter(alToTake);
 
 
@@ -216,7 +216,7 @@ namespace DiscothequeTest
 
         private static void AddAboForTests(string nom, string prenom, string login, string mdp, int codePays)
         {
-            Utils.RegisterAbo(nom, prenom, login, mdp, codePays);
+            _ = Utils.RegisterAbo(nom, prenom, login, mdp, codePays);
         }
 
         private static void SuppAboAfterTests(ABONNÉS abo)
