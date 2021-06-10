8e54a778c09113f6eb0cc2d736ad420257511fce
skhalifa <skhalifa@etu.u-bordeaux.fr>
2021-06-10 10:14:41 +0200

    Ajout méthode pour retourner le string SHA-256 d'un autre string

--- PT2-F/WindowsFormsApp1/MyAbonnes.cs
+++ PT2-F/WindowsFormsApp1/MyAbonnes.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -151,6 +152,24 @@ namespace WindowsFormsApp1
                 return (int)(n + 0.5);
             }
             return (int)(n - 0.5);
+        }
+
+        public static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
 
     }
