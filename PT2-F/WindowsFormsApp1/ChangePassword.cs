c4a256a0e3f8c09410ebc398707a17e1cc2bf761
skhalifa <skhalifa@etu.u-bordeaux.fr>
2021-06-10 14:20:43 +0200

    ChangePassword en travail

---
+++ PT2-F/WindowsFormsApp1/ChangePassword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class ChangePassword : Form
+    {
+        public ChangePassword(ABONNÉS a)
+        {
+            InitializeComponent();
+            radioButton1.Image = Utils.ResizeImage(radioButton1.Image, radioButton1.Width, radioButton1.Height);
+        }
+    }
+}
