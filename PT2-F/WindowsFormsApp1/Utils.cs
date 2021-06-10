e9c7ed13110cdc1e3317baefae9d6f60f9d0bbe6
skhalifa <skhalifa@etu.u-bordeaux.fr>
2021-06-10 14:20:43 +0200

    ChangePassword en travail

--- PT2-F/WindowsFormsApp1/Utils.cs
+++ PT2-F/WindowsFormsApp1/Utils.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
@@ -189,7 +191,7 @@ namespace WindowsFormsApp1
                     select a).FirstOrDefault();
         }
 
-        public static ABONNÉS GetABONNÉS(string login)
+        public static ABONNÉS GetABONNÉ(string login)
         {
             return (from a in Connexion.ABONNÉS
                     where a.LOGIN_ABONNÉ == login
@@ -233,6 +235,38 @@ namespace WindowsFormsApp1
                 }
                 return builder.ToString();
             }
+        }
+
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
         }
 
     }
